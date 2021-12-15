8cfa93e174d6591ca2c0a882a016914fd8a963a9
nakata122 <redspot122@gmail.com>
2021-12-15 14:37:58 +0200

    Color change and filtering

--- Front_end/public/index.js
+++ Front_end/public/index.js
@@ -23,47 +23,40 @@ let stop = false;
 let realTimePlot = null;
 
 ws.addEventListener('message', (msg) => {
-    // some tests
-    // console.log('DATA TYPE: ' + typeof msg.data)
-    // console.log(`New message from server: ${msg.data}.`);
     const parsedData = JSON.parse(msg.data);
     // console.log(parsedData);
-    data.push(parsedData.A);
-    // data.push(Math.random() * 800);
-
-    // ctx.clearRect((index % 100) * 5, -100, 30, 200);
-    // if((index+1) % 100 == 0) {
-    //     ctx.clearRect(-10, -100, 20, 200);
-    //     index++;
-    // }
-
-    // ctx.beginPath();
-    // ctx.moveTo((index % 100) * 5, +data[index]);
-    // ctx.lineTo(((index+1) % 100) * 5, +data[index + 1]);
-    // ctx.stroke();
-    // index++;
+    data.push(+parsedData.A);
 });
 
 let maxVal = 1000;
 let windowWidth = canvas.width;
-let width = 5;
+let width = 1;
+let ECGFilt = [];
 
 let interval = setInterval(() => {
-    // data.push(Math.random() * 100);
-    // console.log(index % 100, data[index]);
+    //Clear partially
     ctx.clearRect((index % Math.floor(windowWidth / width)) * width, -1000, 30, 2000);
     if((index+1) % Math.floor(windowWidth / width) == 0) {
         ctx.clearRect(-10, -1000, 30, 2000);
         // data.push(Math.random() * 100);
         index++;
     }
-    if(data[index] > maxVal) maxVal = data[index];
+    
+    //FILTRATION
+    let data1=0, data2=0;
+    for(let i=0; i < 5; i++) {
+        data1 += data[index + i];
+        data2 += data[index + i + 1];
+    }
+    data1 /= 5;
+    data2 /= 5;
 
-    ctx.strokeStyle = "green"; 
+    //DRAW LINES
+    ctx.strokeStyle = "black"; 
     ctx.lineWidth = 3; 
     ctx.beginPath();
-    ctx.moveTo((index % Math.floor(windowWidth / width)) * width, maxVal - data[index]);
-    ctx.lineTo(((index+1) % Math.floor(windowWidth / width)) * width, maxVal - data[index + 1]);
+    ctx.moveTo((index % Math.floor(windowWidth / width)) * width, maxVal - data1);
+    ctx.lineTo(((index+1) % Math.floor(windowWidth / width)) * width, maxVal - data2);
     ctx.stroke();
     index++;
 }, 10);
@@ -78,10 +71,10 @@ function drawGrid() {
     const dpr = window.devicePixelRatio;
     // let mmSize = window.screen.width / 34.02 / 10 * width;
     let mmSize = canvas.width / 17.02 / 5; //34.02cm is just my const width of the laptop. It varies on each device
-    ctx.strokeStyle = "grey";  
+    ctx.strokeStyle = "red";  
 
     for(let i=0; i < 200; i++) {
-        if(i % 5 === 0) ctx.lineWidth = 1;
+        if(i % 5 === 0) ctx.lineWidth = 2;
         else ctx.lineWidth = 0.5;
 
         ctx.beginPath();
