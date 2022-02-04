51a01c727d58b597eed304924324d0aead63d310
Vladi Markova <vladislavam1996@gmail.com>
2022-02-04 15:02:28 +0200

    change view section added in online mode also, but without poincare option

--- Front_end/public/toggleViewSection.js
+++ Front_end/public/toggleViewSection.js
@@ -1,9 +1,9 @@
 // Toggle view section on offline mode activation and file loaded
 
 export default function toggleViewSection() {
-    const extraInfoElement = document.querySelector('#extra-info');
-    const viewSectionTemplate = document.querySelector('#view-section-template');
+    const poincareViewTemplate = document.querySelector('#poincare-view-template');
+    const changeViewElement = document.querySelector('#change-view-dropdown');
 
-    const viewSection = viewSectionTemplate.content.cloneNode(true);
-    extraInfoElement.appendChild(viewSection);
+    const poincareView = poincareViewTemplate.content.cloneNode(true);
+    changeViewElement.appendChild(poincareView);
 };
